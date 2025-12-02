7231b6024ae35abda74754783299bc2b5f35c4d7
yhsben88 <yhsben@gmail.com>
2025-12-02 14:31:15 -0700

    fix: fix data importing bugs

--- unzip-data.py
+++ unzip-data.py
@@ -1,6 +1,6 @@
 from zipfile import ZipFile
-FILE_DIR = "recyclable-and-household-waste-classification.zip"
-TARGET_DIR = "dataset"
+FILE_DIR = "data/recyclable-and-household-waste-classification.zip"
+TARGET_DIR = "data"
 with ZipFile(FILE_DIR, 'r') as zip:
     zip.extractall(path=TARGET_DIR)
     print(f"SUCCESS: data unzipped to file directory: {TARGET_DIR}")
